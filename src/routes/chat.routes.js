20fab3783356aa81095a5fdf11ad5fd6802bf2e4
PG <prajay.goyani@yahoo.com>
2026-03-15 00:14:13 +0530

    docs: Add notes on chat implementation

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -5,23 +5,42 @@ import User from '../models/user.model.js';
 import Chat from '../models/chat.model.js';
 import Message from '../models/message.model.js';
 
+import { validate } from '../utils/validation.js';
+
 // Get chat listing
 router.get('/', async (req, res) => {
-    // TODO: Add logic to get chat listing
+    const chats = await Chat.find({ cretatedBy: req.user._id })
+        .populate('reciverId', 'name email avatar_url');
+    res.json(chats);
 });
 
+const createChatSchema = {
+    reciverId: { type: 'string', required: true }
+};
+
 // Create chat
-router.post('/', async (req, res) => {
-    // TODO: Add logic to create chat
+router.post('/', validate(createChatSchema), async (req, res) => {
+    const { reciverId } = req.body;
+    const chat = new Chat({
+        reciverId,
+        cretatedBy: req.user._id
+    });
+
+    await chat.save();
+    res.status(201).json(chat);
 });
 
 // Update chat
 router.put('/:id', async (req, res) => {
+    // NOTE: Not in scope right now, we have only one-to-one chat,
+    // so no need to update chat, we can only update messages
     // TODO: Add logic to update chat
 });
 
 // Delete chat
 router.delete('/:id', async (req, res) => {
+    // NOTE: Can we add here soft delete
+    // or we also need to delete all messages related to this chat
     // TODO: Add logic to delete chat with chat messages
 });
 
