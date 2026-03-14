fa2796921e999d8eb2d6b5be5c6af44619a5f151
PG <prajay.goyani@yahoo.com>
2026-03-14 12:05:11 +0530

    feat: schema draft for message

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -6,8 +6,18 @@ const messageSchema = new Schema({
     contentBody: { type: String, default: '', trim: true },
     attachement: {
         kind: { type: String, enum: ['image', 'audio', 'video'], default: null },
-        url: { type: String, default: null }
-    }
+        url: { type: String, default: null },
+        // metadata: { type: Schema.Types.Mixed, default: null }
+    },
+    // status: { type: String, enum: ['sent', 'delivered', 'read'], default: 'sent' },
+    // deliveredAt: { type: Date, default: null },
+    // readAt: { type: Date, default: null },
+    // isEdited: { type: Boolean, default: false },
+    // editedAt: { type: Date, default: null },
+    // isDeleted: { type: Boolean, default: false },
+    // deletedAt: { type: Date, default: null },
+    // sentAt: { type: Date, default: Date.now }, // - can refer to createdAt
+    createdAt: { type: Date, default: Date.now },
 });
 
 const Message = mongoose.model('Message', messageSchema);
