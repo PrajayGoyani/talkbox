82d35e657b9486c6cf8c5a99dc3017ff6176ddc1
PG <prajay.goyani@yahoo.com>
2026-04-06 15:18:49 +0530

    chore: add Render deployment configurations and update frontend API resolution logic

--- front-end/src/lib/config.ts
+++ front-end/src/lib/config.ts
@@ -9,6 +9,10 @@ const getBaseUrl = (): string => {
   if (typeof window === 'undefined') return 'http://localhost:3000'; // SSR fallback
 
   const { hostname } = window.location;
+  // render specific check
+  if (hostname.startsWith('user-chat')) {
+    return 'https://user-chat-rg8u.onrender.com'
+  }
   // if (hostname === 'localhost' || hostname === '127.0.0.1') {
   //   return 'http://localhost:3000';
   // }
@@ -18,7 +22,7 @@ const getBaseUrl = (): string => {
 /** Root server URL – use for WebSocket connections */
 export const API_ROOT = getBaseUrl();
 
-console.log({API_ROOT });
+// console.log({ API_ROOT });
 
 /** REST API base – use for fetch calls */
 export const API_BASE = `${API_ROOT}/api`;
